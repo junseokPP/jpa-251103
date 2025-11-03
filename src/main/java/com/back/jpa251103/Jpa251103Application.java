fe31783ef565d3b1f3500b85fb582c7b1d8fa029
junseokPP <pjs981130@gmail.com>
2025-11-03 20:36:55 +0900

    start

---
+++ src/main/java/com/back/jpa251103/Jpa251103Application.java
@@ -0,0 +1,13 @@
+package com.back.jpa251103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa251103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa251103Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/jpa251103/Jpa251103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.jpa251103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa251103ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
