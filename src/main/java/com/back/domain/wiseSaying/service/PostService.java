ea92e0d7074f6f044f001f93f7db1fb16b3dfde7
junseokPP <pjs981130@gmail.com>
2025-11-04 20:52:15 +0900

    21,22,23강

---
+++ src/main/java/com/back/domain/wiseSaying/service/PostService.java
@@ -0,0 +1,30 @@
+package com.back.domain.wiseSaying.service;
+
+import com.back.domain.wiseSaying.entity.Post;
+import com.back.domain.wiseSaying.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+
+    public void write(String title, String content) {
+        Post post1 = new Post(title,content);
+        postRepository.save(post1);
+    }
+
+
+    public Long count() {
+       return postRepository.count();
+    }
+
+    public Optional<Post> getPost(int i) {
+        return postRepository.findById(i);
+    }
+}
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,39 +1,40 @@
 package com.back.global.initData;
 
 import com.back.domain.wiseSaying.entity.Post;
-import com.back.domain.wiseSaying.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.back.domain.wiseSaying.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @Configuration
+@RequiredArgsConstructor
 public class BaseInitData {
 
-    @Autowired
-    private PostRepository postRepository;
+    private final PostService postService;
 
     @Bean
     ApplicationRunner initData(){
         return args -> {
 
-            if(postRepository.count()>0){
-                return;
-            }
 
             work1();
             work2();
+
         };
     }
 
     private void work2() {
-        postRepository.findById(1);
+        Optional<Post> post = postService.getPost(1);
     }
 
-    private void work1() {
-        Post post1 = new Post("제목1","내용1");
-        postRepository.save(post1);
-        Post post2 = new Post("제목2","내용2");
-        postRepository.save(post2);
+    public void work1(){
+        if(postService.count()>0){
+            return;
+        }
+        postService.write("제목1","내용1");
+        postService.write("제목2","내용2");
     }
 }
