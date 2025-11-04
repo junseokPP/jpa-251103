b20c35342a01635fb10fce3f3a8d91f17ae0f2be
junseokPP <pjs981130@gmail.com>
2025-11-04 19:50:51 +0900

    14강 : @RequiredArgsConstructor는 부분생성자들을 만들어주는데 식별하는 단위가 final이다.

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -7,21 +7,21 @@ import lombok.*;
 @Setter
 @ToString
 @Entity
+@RequiredArgsConstructor
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String title;
+    private final String title;
 
     @Column(columnDefinition = "TEXT")
-    private String content;
+    private final String content;
 
-    public Post() {}
-
-    public Post(String title, String content) {
-        this.title = title;
-        this.content = content;
+    public Post(){
+        this.title = "";
+        this.content = "";
     }
+
 }
