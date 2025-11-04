3a5fa316dc1fdb23a1ff281638278e3fab9e9e0e
junseokPP <pjs981130@gmail.com>
2025-11-04 15:37:15 +0900

    1-강 : PostRepository 생성

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,8 @@
 package com.back.global.initData;
 
+import com.back.domain.wiseSaying.entity.Post;
+import com.back.domain.wiseSaying.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +10,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BaseInitData {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Bean
     ApplicationRunner initData(){
         return args -> {
             System.out.println("초기 데이터를 로딩합니다.");
+
+            Post post = new Post();
+            post.setTitle("첫번째 제목");
+            post.setContent("첫번째 게시글을 업로드 합니다.");
+
         };
     }
 }
